90efc043e75dfe9ecb9d0b02fa2145319ef67862
Thecodemaster21-j <josuer334@icloud.com>
2025-04-29 20:56:57 -0700

    mock portfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+// a little feedback code that shows up in the console when you insepct the page
+let feedback = [];
+
+function addFeedback(message) {
+  feedback.push(message);
+  return feedback;
+}
+console.log(addFeedback("Great website!")); // ["Great website!"]
+// A mock email validate that shows true in the console
+function validateEmail(email) {
+  return email.includes("@") && email.includes(".");
+}
+console.log(validateEmail("test@example.com")); // true
+
